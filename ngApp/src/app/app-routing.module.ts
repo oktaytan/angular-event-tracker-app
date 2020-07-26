a539e603cc0bd52f03f9f3a34e6b290bb63830d3
Oktay Tanrıkulu <oktaytanrikulu@gmail.com>
2020-07-26 19:17:27 +0300

    Added components and edited routing module

--- ngApp/src/app/app-routing.module.ts
+++ ngApp/src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
-import { NgModule } from '@angular/core';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { SpecialEventsComponent } from './special-events/special-events.component';
+import { EventsComponent } from './events/events.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/events',
+    pathMatch: 'full',
+  },
+  {
+    path: 'events',
+    component: EventsComponent,
+  },
+  {
+    path: 'special',
+    component: SpecialEventsComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
